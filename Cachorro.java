40664d1a7ed216d95ef602fee6103db249ff7e05
Paulo Vanderley <vanderley.1109@gmail.com>
2023-06-23 14:20:05 -0300

    adicionando conteudo

---
+++ Cachorro.java
@@ -0,0 +1,25 @@
+package br.com.devsgeeknerd.zoologico.classes;
+
+public class Cachorro extends Animal {
+    public Cachorro(String nome) {
+        super(nome);
+    }
+
+    public Cachorro(String nome, int idade, String especie) {
+        super(nome, idade, especie);
+    }
+
+    public Cachorro(String nome, int idade) {
+        super(nome, idade, "Cachorro");
+    }
+
+    @Override
+    public void emitirBarulho() {
+        if (estaVivo) {
+            System.out.println("Latido do cachorro");
+        } else {
+            System.out.println("O animal morreu!");
+        }
+    }
+
+}
